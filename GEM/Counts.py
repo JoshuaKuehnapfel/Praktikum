f162beefcebd53d8b9d54072bdb1d1a84ef2760a
Joshua Kuehnapfel <Joshua.Kuehnapfel@gmail.com>
2026-03-13 20:51:35 +0100

    Merge conflict resolve

--- GEM/Counts.py
+++ GEM/Counts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import os
 os.chdir("Praktikum/GEM/Daten")
@@ -10,10 +9,3 @@ for n in name:
     data = np.loadtxt(n + 'spectrum.csv', delimiter=',', skiprows=1)-data_rausch
     print(n)
     print('Summe: ', np.sum(data).round(2))
-=======
-import numpy as np
-
-daten = np.loadtxt('GEM/Daten/Eisenspektrum.csv', skiprows = 1, delimiter = ',')
-summe = np.sum(daten[1,:])
-print('Die Summe der Zählraten beträgt: ', summe)
->>>>>>> 4459621dedf4e1a401f57a4774b5e6cb8f78cd3b
